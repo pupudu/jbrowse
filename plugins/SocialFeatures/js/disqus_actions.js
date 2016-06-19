08317770e790aa8e015885934a996a081ec9dfcb
Pubudu Dodangoda <Pubudu.Dodangoda@trycake.com>
2016-06-19 21:02:43 +0530

    Modularize social features plugin
    -Move sidebar css from index html to a separate stylesheet
    -Fix reload comments issue
    -Declare separate functions to load exisiting threads and start new threads

--- plugins/SocialFeatures/js/disqus_actions.js
+++ plugins/SocialFeatures/js/disqus_actions.js
@@ -1,28 +1,39 @@
 /**
-* RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-* LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+*  Load initial comment thread on site load
 */
-
 var disqus_config = function () {
-  this.page.identifier = "identifier_5";   // Not usefull in the context of JBrowse
+  this.page.identifier = "identifier_1";   
 };
 
-function reload(id_num){
+/**
+* Load exisiting thread and refresh disqus comments section
+*/
+function reload(identifier){
   DISQUS.reset({
-  reload: true,
-  config: function () {  
-    //alert(this.page.url);
-    this.page.identifier = "identifier_"+id_num;   // Not usefull in the context of JBrowse
-    // this.page.url = "http://sid.projects.mrt.ac.lk/id_test_3";
-    // this.page.title = "New Thread id_test_3";
-  }
+    reload: true,
+    config: function () {  
+      this.page.identifier = identifier;  
+    }
   });
 }
+
+/**
+* Start a new thread and refresh disqus comment section
+*/
+function startThread(url,identifier,title){
+  DISQUS.reset({
+    reload: true,
+    config: function () {  
+      this.page.identifier = identifier;   
+      this.page.url = url;
+      this.page.title = title;
+    }
+  });
+}
+
 (function() { // DON'T EDIT BELOW THIS LINE
-var d = document, s = d.createElement('script');
-
-s.src = '//jbrowse.disqus.com/embed.js';
-
-s.setAttribute('data-timestamp', +new Date());
-(d.head || d.body).appendChild(s);
+  var d = document, s = d.createElement('script');
+  s.src = '//jbrowse.disqus.com/embed.js';
+  s.setAttribute('data-timestamp', +new Date());
+  (d.head || d.body).appendChild(s);
 })();
